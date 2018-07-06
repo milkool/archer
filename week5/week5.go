bde3ba3145e4b1e4d360ec1c228289d52e84c13d
archer_hsu <archer_hsu@mail.chungyo.net>
2018-07-06 16:46:57 +0800

    week5

---
+++ week5/week5.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main()  {
+	//設定限制數值
+	const limit int = 20
+	//記錄開始時間
+	start := time.Now()
+
+	//建立通道與buff
+	c := make(chan int64, limit)
+	//用goroutine運算各階乘
+	for i := 1; i<=limit; i++ {
+		go factorial(i, c)
+	}
+
+	var anser int64
+	//接收階乘結果並相加
+	for i:=0; i<limit; i++{
+		select {
+		case res := <- c:
+			anser = anser + res
+		}
+	}
+
+	//紀錄消耗時間
+	end := time.Since(start)
+	fmt.Println(end)
+	fmt.Println(anser)
+}
+
+func factorial(n int, c chan int64)  {
+	var res int64 = 1
+	for i := n; i > 0; i -- {
+		res = res * int64(i)
+	}
+	//fmt.Printf("%d!= %d\n", n, res)
+	c <- res
+}
