cbcc8d98c1ac574b923853b7a41bad71ff08e168
archer_hsu <archer_hsu@mail.chungyo.net>
2018-07-19 18:39:01 +0800

    week6

--- week6/hw2/main.go
+++ week6/hw2/main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
-	archer "archer"
+	"archer"
+	"fmt"
+	"time"
+	"strconv"
 )
 
+type Rocket struct {
+	Name string
+	Fuel int
+}
+
+func (r Rocket) Launch() {
+	fmt.Println(r.Name + " 啟動發射程序")
+	time.Sleep(1 * time.Second)
+
+	for i := 10; i>=0; i-- {
+		fmt.Println(r.Name + " 發射倒數 " + strconv.Itoa(i))
+		time.Sleep(1 * time.Second)
+	}
+
+	fmt.Println(r.Name + " 發射!!!")
+	time.Sleep(2 * time.Second)
+}
+
+
 func main()  {
-	rocket1 := archer.Rocket{Name:"洽洽號", Fuel:3}
-	rocket2 := archer.Rocket{Name:"粉紅惡魔兔", Fuel:5}
-	rocket1.Launch(rocket1)
-	rocket2.Launch(rocket2)
+	rocket1 := Rocket{Name:"洽洽號", Fuel:3}
+	rocket2 := Rocket{Name:"DDD號", Fuel:3}
+	archer.Start(rocket1)
+	archer.Start(rocket2)
 }
